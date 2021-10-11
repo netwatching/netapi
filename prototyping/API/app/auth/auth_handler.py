be0849a51c440ff216b3bd670c4aea62a896d5b8
Peter Lassnig <peter.lassnig@outlook.at>
2021-10-11 16:42:50 +0200

    code quality

--- prototyping/API/app/auth/auth_handler.py
+++ prototyping/API/app/auth/auth_handler.py
@@ -4,7 +4,6 @@ from typing import Dict
 import jwt
 from decouple import config
 
-
 JWT_SECRET = config("secret")
 JWT_ALGORITHM = config("algorithm")
 
@@ -14,7 +13,8 @@ def token_response(token: str):
         "access_token": token
     }
 
-def signJWT(user_id: str) -> Dict[str, str]:
+
+def sign_jwt(user_id: str) -> Dict[str, str]:
     payload = {
         "user_id": user_id,
         "expires": time.time() + 600
@@ -23,9 +23,10 @@ def signJWT(user_id: str) -> Dict[str, str]:
 
     return token_response(token)
 
-def decodeJWT(token: str) -> dict:
+
+def decode_jwt(token: str) -> dict:
     try:
         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
         return decoded_token if decoded_token["expires"] >= time.time() else None
-    except:
+    finally:
         return {}
