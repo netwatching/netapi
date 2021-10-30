8baae703abc4deb0bff4fe1b9ea5e86867d995e1
Peter Lassnig <peter.lassnig@outlook.at>
2021-10-30 16:22:00 +0200

    added frontend APIs for existing DBs

--- prototyping/API/app/frontend.py
+++ prototyping/API/app/frontend.py
@@ -1,21 +1,29 @@
 from fastapi import FastAPI, Body, Depends, Request
 import json
-from models import Device
+from app.models import Device
 from app.auth.auth_bearer import JWTBearer
 from app.auth.auth_handler import sign_jwt
 from decouple import config
+import pymongo
 
 
 app = FastAPI()
+db = pymongo.MongoClient("mongodb://root:testPassword1234@palguin.htl-vil.local:27017")
 
 
 @app.get("/", dependencies=[Depends(JWTBearer())])
 async def read_root() -> dict:
+    """
+    / - GET - for API testing purposes only
+    """
     return {"message": "Welcome to Auth."}
 
 
-@app.post("/login")
+@app.post("/api/frontend/login")
 async def login_user(req: Request):
+    """
+    /login - POST - authenticates frontend User and returns JWT
+    """
     json_body = await req.json()
     json_body['pw'] = config("pw")
     return sign_jwt(json_body['id'])
@@ -26,15 +34,25 @@ async def get_all_devices():
     """
     /devices - GET - get all devices for the frontend
     """
+    db_col = db["zabix"]["devices"]
     devices = []
-    for i in range(1, 10):
-        d = Device(
-            id=i,
-            name=f'demoDevice{i}',
-            ip=f'1.1.1.{i}',
-            type='Cisco' if i % 2 else 'Ubi',
-            aggregator_id=i,
-            timeout=i)
-        devices.append(d.serialize())
-    return {"devices": devices}
+
+    for x in db_col.find({}, {"_id": 1, "name": 1, "timestamp": 1}):
+        devices.append(x)
+
+    return devices
+
+@app.get("/api/frontend/devices/problems", dependencies=[Depends(JWTBearer())])
+async def get_all_problems():
+    """
+    /devices/problems - GET - get all problems of the devices for the frontend
+    """
+    db_col = db["zabix"]["devices"]
+    devices = []
+
+    for x in db_col.find({}, {"_id": 1, "problem": 1,}):
+        x["device_id"] = x.pop('_id')
+        devices.append(x)
+
+    return devices
 
