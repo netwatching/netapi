6b9c3a056b3c62254bae6ef670fed1d52ac77675
Peter Lassnig <peter.lassnig@outlook.at>
2021-12-07 09:09:16 +0100

    added ssl support

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import uvicorn
+
+import asyncio
+
+from src.api import app
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 
 if __name__ == "__main__":
-    uvicorn.run("src.api:app", host="0.0.0.0", port=8081, reload=True)
+    cfg = Config()
+    cfg.bind = ["0.0.0.0:8443"]
+    cfg.insecure_bind = ["0.0.0.0:8080"]
+    cfg.keyfile = "./ssl/palguin.htl-vil.local+3-key.pem"
+    cfg.certfile = "./ssl/palguin.htl-vil.local+3.pem"
+
+    asyncio.run(serve(app, cfg))
