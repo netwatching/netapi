a73f5bc72ed3a384db069cefea9298a54dd794ed
Thomas Reiter <reitert1@edu.htl-villach.at>
2022-01-18 11:27:42 +0100

    added demo devices (ubi and cisco)

--- src/api.py
+++ src/api.py
@@ -115,6 +115,24 @@ async def aggregator(id: int, authorize: AuthJWT = Depends()):
         d = oldDevice(id=2, name=f'Ubi', ip=f'172.31.37.95', type='Ubiquiti', aggregator_id=id, timeout=10,
                       module_name=['snmp'])
         out.append(d.serialize())
+        d = oldDevice(id=3, name=f'Ubi', ip=f'172.31.8.81', type='Cisco', aggregator_id=id, timeout=10,
+              module_name=['snmp'])
+        out.append(d.serialize())
+        d = oldDevice(id=4, name=f'Ubi', ip=f'172.31.37.89', type='Ubiquiti', aggregator_id=id, timeout=10,
+              module_name=['snmp'])
+        out.append(d.serialize())
+        d = oldDevice(id=5, name=f'Ubi', ip=f'172.31.37.70', type='Ubiquiti', aggregator_id=id, timeout=10,
+              module_name=['snmp'])
+        out.append(d.serialize())
+        d = oldDevice(id=6, name=f'Ubi', ip=f'172.31.37.78', type='Ubiquiti', aggregator_id=id, timeout=10,
+              module_name=['snmp'])
+        out.append(d.serialize())
+        d = oldDevice(id=7, name=f'Ubi', ip=f'172.31.37.46', type='Ubiquiti', aggregator_id=id, timeout=10,
+              module_name=['snmp'])
+        out.append(d.serialize())
+        d = oldDevice(id=8, name=f'Ubi', ip=f'172.31.37.44', type='Ubiquiti', aggregator_id=id, timeout=10,
+              module_name=['snmp'])
+        out.append(d.serialize())
     return out
 
 
