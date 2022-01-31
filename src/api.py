37894cb7ec649dcac879cf2d844212ab4ea815b2
Tobias <Tobias@DESKTOP-MELU9RI.local>
2022-01-31 06:27:29 -0800

    Update api.py

--- src/api.py
+++ src/api.py
@@ -474,12 +474,12 @@ async def redis(request: Request, authorize: AuthJWT = Depends()):
     )
 
     # {
-    #   "device": "TESTING",
-    #   "data": {
-    #     "in_unicast_packets": {
-    #     "2022-01-23 21:00:00": "2000"
+    #     "device": "TESTING",
+    #     "data": {
+    #         "in_bytes": {
+    #             "2022-01-23 21:00:00": "2000"
+    #         }
     #     }
-    #   }
     # }
 
 
