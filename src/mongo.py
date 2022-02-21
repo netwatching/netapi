81a933dcc27c52f8c73c3e73df93bbecb6fcbf13
Tobias <Tobias@DESKTOP-MELU9RI.local>
2022-02-21 06:25:42 -0800

    Update mongo.py

--- src/mongo.py
+++ src/mongo.py
@@ -1,7 +1,8 @@
 import json
 from datetime import datetime
 
-import pymodm
+from pymodm import connection
+from pymongo import DESCENDING
 import pymongo.errors
 
 from src.models.event import Event
@@ -12,7 +13,7 @@ from src.models.device import Device
 class Mongo:
     # def __init__(self, details):
     #    self.client = pymodm.connection.connect(details)
-    pymodm.connection.connect(
+    connection.connect(
         "mongodb://netwatch:jfMCDp9dzZrTxytB6zSrtEjkqXcrmvPKrnXttTFj383u8UFmN3AqY9XdPw7H@palguin.htl-vil.local:27017/netdb?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false")
 
     if __name__ == "__main__":
@@ -50,3 +51,6 @@ class Mongo:
             print(e)
         except Exception as e:
             print(e)
+
+    def get_modules(self):
+        return list(Module.objects.order_by(['type', DESCENDING]).all())
