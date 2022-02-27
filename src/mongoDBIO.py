1ba0b3b5070ef2ec35fe3c26536e4ecdb632d762
Tobias <Tobias@DESKTOP-MELU9RI.local>
2022-02-27 10:58:52 -0800

    Update mongoDBIO.py

--- src/mongoDBIO.py
+++ src/mongoDBIO.py
@@ -1,9 +1,6 @@
-import json
-from datetime import datetime
-
+from fastapi import HTTPException
 from pymodm import connection
 from pymongo import DESCENDING
-import pymongo.errors
 
 from src.models.event import Event
 from src.models.aggregator import Aggregator
@@ -17,18 +14,16 @@ class MongoDBIO:
         connection.connect(details)
 
     def get_modules(self):
-        try:
-            modules = list(Module.objects.order_by([['type', DESCENDING]]).all())
-            return modules
-        except Exception as e:
-            print(e)
+        modules = list(Module.objects.order_by([['type', DESCENDING]]).all())
+        return modules
 
     def add_category(self, category: str):
         try:
             category = Category(category=category).save()
             return category
-        except Exception as e:
-            print(e)
+        except Category.DuplicateKeyError:
+            return False
+
 
     def add_device(self, hostname: str, category: Category, ip: str = None):
         try:
@@ -37,8 +32,8 @@ class MongoDBIO:
                 ip=ip,
                 category=category).save()
             return device
-        except Exception as e:
-            print(e)
+        except Device.DuplicateKeyError:
+            return False
 
     def check_token(self, token: str):
         try:
@@ -53,15 +48,25 @@ class MongoDBIO:
         try:
             ag = Aggregator(token=token).save()
             return ag
-        except Exception as e:
-            print(e)
+        except Aggregator.DuplicateKeyError:
+            return False
 
-# https://stackoverflow.com/questions/46366398/how-to-convert-pymodm-objects-to-json
+    # https://stackoverflow.com/questions/46366398/how-to-convert-pymodm-objects-to-json
     def get_aggregator_devices(self, id: str):
         try:
             ag = Aggregator.objects.get({'_id': id})
             return ag.devices.to_son().to_dict()
         except Aggregator.DoesNotExist:
-            return
+            return False
         except Aggregator.MultipleObjectsReturned:
-            return
+            return -1
+
+    def get_device_by_id(self, id: str):
+        try:
+            device = Device.objects.get({'_id': id})
+            return device
+        except Device.DoesNotExist:
+            return False
+        except Device.MultipleObjectsReturned:
+            return -1
+
