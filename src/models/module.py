7a0345b7d4f643b32390af95e039889b1606ef84
steigert <steigert@edu.htl-villach.at>
2022-03-13 00:37:39 +0100

    Update module.py

--- src/models/module.py
+++ src/models/module.py
@@ -1,10 +1,16 @@
 from pymodm import MongoModel, fields, ReferenceField
+from pymongo import IndexModel, DESCENDING
 
 
 class Type(MongoModel):
     type = fields.CharField(required=True)
     config = fields.CharField(required=True)
     signature = fields.CharField(required=True)
+
+    class Meta:
+        indexes = [
+            IndexModel([('type', DESCENDING)], unique=True)
+        ]
 
 
 class Module(MongoModel):
