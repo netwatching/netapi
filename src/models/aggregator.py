bfbde8ec809428ad991c63bab6879d5d6880cc77
steigert <steigert@edu.htl-villach.at>
2022-03-13 13:11:03 +0100

    Update aggregator.py

--- src/models/aggregator.py
+++ src/models/aggregator.py
@@ -7,7 +7,7 @@ from src.models.module import Type
 class Aggregator(MongoModel):
     token = fields.CharField(required=True)
     version = fields.CharField(required=False)
-    identifier = fields.CharField(required=False)
+    identifier = fields.CharField(required=True)
     devices = fields.ListField(fields.ReferenceField(Device, on_delete=ReferenceField.NULLIFY))
     types = fields.ListField(fields.ReferenceField(Type, on_delete=ReferenceField.NULLIFY))
 
