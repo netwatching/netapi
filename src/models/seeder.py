840b928089e8350175adcea5f8ada0b81021c91b
Steigert <Steigert@DESKTOP-0V5KIPQ.local>
2022-03-15 14:06:38 -0700

    Update seeder.py

--- src/models/seeder.py
+++ src/models/seeder.py
@@ -6,11 +6,11 @@ from decouple import config
 from faker import Faker
 from pymodm import connection
 
-from src.models.device import Category, Data
+from src.models.device import Category, Data, Device
 
 details = f'mongodb://{config("mDBuser")}:{config("mDBpassword")}@{config("mDBurl")}:{config("mDBport")}/{config("mDBdatabase")}?authSource=admin'
 connection.connect(details)
-faker = Faker()
+fake = Faker()
 
 # Counts
 category_count = 100
@@ -26,13 +26,11 @@ device_ip_template = "test_domain_or_ip_{}"
 
 # Static System
 static_data_system_template = {
-    "system": {
-        # "name": "HTL-R154-PoE-Access",
-        "uptime": 3019345000,
-        "description": "SG350X-48MP 48-Port Gigabit PoE Stackable Managed Switch",
-        "contact": "florian.schmidt@htl-villach.at",
-        "location": "HTL-R154-PoE-Access"
-    }
+    # "name": "HTL-R154-PoE-Access",
+    "uptime": 3019345000,
+    "description": "SG350X-48MP 48-Port Gigabit PoE Stackable Managed Switch",
+    "contact": "florian.schmidt@htl-villach.at",
+    "location": "HTL-R154-PoE-Access"
 }
 
 # Static Interface
@@ -79,35 +77,92 @@ def seed_categories(count):
     return categories
 
 
-def seed_static_data(count):
+def seed_static_data(count, macs):
     static_data = []
 
     system = static_data_system_template
-    system["name"] = faker.domain_word()
-    static_data.append(Data(key="system", value=system).save())
+    system["name"] = fake.domain_word()
+    system = {
+        "system": system
+    }
+    static_data.append(Data(key="system", data=system).save())
 
-    try:
-        sourc_mac = faker.unique.mac_address()
-        target
-    except faker.exceptions.UniquenessException:
-        return
-
+    interfaces = {}
+    neighbors = {}
+    currently_used_macs = []
     for i in range(0, count):
-        interface = static_data_interface_template
+        interface = dict(static_data_interface_template)
         interface["index"] = str(i)
         interface["port"] = str(i)
-        interface["description"] = f"GigabitEthernet{i}"
-        interface["mac_address"] = str(sourc_mac)
+        interface["description"] = f"GigabitEthernet{str(i)}"
+
+        random.shuffle(macs)
+        for mac in macs:
+            if mac["used"] is False:
+                interface["mac_address"] = mac["mac"]
+                mac["used"] = True
+                currently_used_macs.append(mac["mac"])
+                break
+
+        neighbor = None
+        if random.randint(1, 100) <= 15:
+            random.shuffle(macs)
+            for mac in macs:
+                if mac["used"] is True and mac["mac"] not in currently_used_macs:
+                    neighbor = static_data_neighbor_template[:]
+                    neighbor[0]["local_port"] = interface["description"]
+                    neighbor[0]["local_mac"] = interface["mac_address"]
+
+                    neighbor[0]["remote_chassis_id"] = mac["mac"]
+                    neighbor[0]["remote_port"] = mac["mac"]
+                    break
+
+        interfaces[interface["description"]] = dict(interface)
+        if neighbor is not None:
+            neighbors[interface["description"]] = neighbor[:]
+
+    static_data.append(Data(key="network_interfaces", data=interfaces).save())
+    if neighbors:
+        static_data.append(Data(key="neighbors", data=neighbors).save())
+    return static_data
 
 
-def seed_devices(count, category):
+def __create_macs__(count):
+    try:
+        macs = []
+        for i in range(0, count):
+            macs.append(
+                {
+                    "mac": fake.unique.mac_address(),
+                    "used": False
+                }
+            )
+    # except faker.exceptions.UniquenessException:
+    #     return macs
+    # return macs
+    finally:
+        return macs
+
+
+def seed_devices(count, port_count, category, macs):
+    for i in range(0, count):
+        Device(
+            hostname=device_hostname_template.format(str(i) + str(category.pk)),
+            ip=device_ip_template.format(str(i) + str(category.pk)),
+            category=category,
+            static=seed_static_data(port_count, macs)
+        ).save()
 
 
 def seeder(category_count, device_count, port_count):
+    clear()
+    Faker.seed(0)
+    fake.unique.clear()
+
+    macs = __create_macs__(category_count * device_count * port_count)
     categories = seed_categories(category_count)
-
     for category in categories:
-        seed_devices(device_count, category)
+        seed_devices(device_count, port_count, category, macs)
 
 
-
+seeder(category_count, device_count, ports_count)
