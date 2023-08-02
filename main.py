037edb2f17f05c02cdacee643caee74a875c6062
root <thamersimon@gmail.com>
2023-08-02 12:45:41 +0200

    cleanup of mysql; disabled redis; support for SIGTERM & SIGSTOP

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import asyncio
 import ssl
+import os
+import signal
+from typing import Any
 
 import pymongo.errors
 
@@ -9,24 +12,36 @@ from hypercorn.config import Config
 from hypercorn.asyncio import serve
 
 
-async def main(config):
-    await asyncio.gather(serve(app, config), MongoDBIO.thread_insertIntoDatabase(mongo), MongoDBIO.keep_connections(mongo))
+cfg = Config()
+cfg.bind = ["0.0.0.0:8443"]
+cfg.insecure_bind = ["0.0.0.0:8080"]
+cfg.keyfile = "./ssl/palguin.htl-vil.local+3-key.pem"
+cfg.certfile = "./ssl/palguin.htl-vil.local+3.pem"
+cfg.worker_class = 'asyncio'
+cfg.accesslog = "-"
+cfg.loglevel = "DEBUG"
+
+
+def main(config):
+    loop = asyncio.new_event_loop()
+
+    shutdown_event = asyncio.Event()
+    def _signal_handler(*_: Any) -> None:
+        shutdown_event.set()
+    loop.add_signal_handler(signal.SIGTERM, _signal_handler)
+    loop.add_signal_handler(signal.SIGINT, _signal_handler)
+
+    while True:
+        try:
+            loop.run_until_complete(serve(app, config, shutdown_trigger=shutdown_event.wait))
+            if shutdown_event.is_set():
+                os._exit(-1)
+            #await asyncio.gather(serve(app, config), MongoDBIO.thread_insertIntoDatabase(mongo), MongoDBIO.keep_connections(mongo), return_exceptions=True)
+        except (pymongo.errors.ServerSelectionTimeoutError, TimeoutError, asyncio.exceptions.CancelledError, ssl.SSLError, OSError):
+            print("Lost connection to DB! Restarting")
 
 def run():
-    cfg = Config()
-    cfg.bind = ["0.0.0.0:8443"]
-    cfg.insecure_bind = ["0.0.0.0:8080"]
-    cfg.keyfile = "./ssl/palguin.htl-vil.local+3-key.pem"
-    cfg.certfile = "./ssl/palguin.htl-vil.local+3.pem"
-    cfg.worker_class = 'asyncio'
-    cfg.accesslog = "-"
-    cfg.loglevel = "DEBUG"
-
     asyncio.run(main(cfg))
 
 if __name__ == "__main__":
-    while True:
-        try:
-            run()
-        except (pymongo.errors.ServerSelectionTimeoutError, TimeoutError, asyncio.exceptions.CancelledError, ssl.SSLError, OSError):
-            print("Lost connection to DB! Restarting")
+    main(cfg)
